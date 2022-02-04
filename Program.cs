4b41f6ba7022e608cc004706a6f5e4de04239d59
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-02-04 17:43:20 -0500

    Shorten name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TwiMLRepl;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<CompileService>();
